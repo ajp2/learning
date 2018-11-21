a1258ab0c8060ac697dc3d45846c0e4c4584b046
Arvind <arvind.puvi@gmail.com>
2018-11-22 00:03:22 +1100

    Fix children method

--- Ruby/W1D5/tictactoe/lib/tic_tac_toe_node.rb
+++ Ruby/W1D5/tictactoe/lib/tic_tac_toe_node.rb
@@ -1,7 +1,7 @@
 require_relative 'tic_tac_toe'
 
 class TicTacToeNode
-  attr_reader :prev_move_pos
+  attr_reader :board, :next_mover_mark, :prev_move_pos
 
   def initialize(board, next_mover_mark, prev_move_pos = nil)
     @board = board
@@ -10,6 +10,10 @@ class TicTacToeNode
   end
 
   def losing_node?(evaluator)
+    if @board.over?
+      return true if @board.winner == evaluator
+      return false if @board.winner != evaluator
+    end
   end
 
   def winning_node?(evaluator)
@@ -20,19 +24,27 @@ class TicTacToeNode
   def children
     children_nodes = []
 
-    @board.each_with_index do |row, idx1|
+    @board.rows.each_with_index do |row, idx1|
       row.each_with_index do |ele, idx2|
         if @board.empty?([idx1, idx2])
           new_board = @board.dup
-          new_board[idx1, idx2] = @next_mover_mark
-          children_nodes << new_board
-
-          @next_mover_mark = @next_mover_mark == :x ? :o : :x
+          
+          new_board[[idx1, idx2]] = @next_mover_mark
+          mark = @next_mover_mark == :x ? :o : :x
           @prev_move_pos = [idx1, idx2]
+
+          children_nodes << TicTacToeNode.new(new_board, mark, @prev_move_pos)
         end
       end
     end
 
+    @next_mover_mark = @next_mover_mark == :x ? :o : :x   
+
     children_nodes
   end
 end
+
+
+b = Board.new
+t = TicTacToeNode.new(b, :x)
+t.children
