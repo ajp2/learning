26d9c5937e100ac395e684ea13a909edaf36cfc3
Arvind <arvind.puvi@gmail.com>
2018-11-22 22:22:45 +1100

    Finish simon

--- Ruby/W2D1/simon/lib/simon.rb
+++ Ruby/W2D1/simon/lib/simon.rb
@@ -4,38 +4,69 @@ class Simon
   attr_accessor :sequence_length, :game_over, :seq
 
   def initialize
-
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
 
   def play
-
+    until game_over
+      take_turn
+    end
+    
+    game_over_message
+    reset_game
   end
 
   def take_turn
+    show_sequence
+    require_sequence
 
+    if !game_over
+      round_success_message
+      self.sequence_length += 1
+    end
   end
 
   def show_sequence
+    add_random_color
 
+    seq.each do |color|
+      system("clear")
+      puts color
+      sleep(1)
+      system("clear")
+    end
   end
 
   def require_sequence
+    puts "Enter the colors in the order they appear, separated by commas: "
+    input = gets.chomp.split(",")
 
+    self.game_over = true if input != seq
   end
 
   def add_random_color
-
+    seq << COLORS.sample
   end
 
   def round_success_message
-
+    puts "You pass this round!"
+    sleep(1)
   end
 
   def game_over_message
-
+    puts "Game over! Incorrect sequence"
   end
 
   def reset_game
-
+    self.sequence_length = 1
+    self.game_over = false
+    self.seq = []
   end
+end
+
+if $PROGRAM_NAME == __FILE__
+  game = Simon.new
+  game.play
 end
