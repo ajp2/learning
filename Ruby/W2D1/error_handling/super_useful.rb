f30221a402853f0b977638a2a03844d201184f4e
Arvind <arvind.puvi@gmail.com>
2018-11-23 23:57:02 +1100

    Complete phase 2 and 3

--- Ruby/W2D1/error_handling/super_useful.rb
+++ Ruby/W2D1/error_handling/super_useful.rb
@@ -1,6 +1,8 @@
 # PHASE 2
 def convert_to_int(str)
   Integer(str)
+rescue ArgumentError => e
+  nil
 end
 
 # PHASE 3
@@ -9,6 +11,8 @@ FRUITS = ["apple", "banana", "orange"]
 def reaction(maybe_fruit)
   if FRUITS.include? maybe_fruit
     puts "OMG, thanks so much for the #{maybe_fruit}!"
+  elsif maybe_fruit == "coffee"
+    raise "Coffee"
   else 
     raise StandardError 
   end 
@@ -19,7 +23,11 @@ def feed_me_a_fruit
 
   puts "Feed me a fruit! (Enter the name of a fruit:)"
   maybe_fruit = gets.chomp
-  reaction(maybe_fruit) 
+  reaction(maybe_fruit)
+
+rescue => e
+  retry if e.message == "Coffee"
+  nil
 end  
 
 # PHASE 4
