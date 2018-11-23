e68c65ca55e2de4e4a8ebd2525cae8e3f63598df
Arvind <arvind.puvi@gmail.com>
2018-11-24 00:53:13 +1100

    Update class inheritance after checking solutions

--- Ruby/W2D1/employee.rb
+++ Ruby/W2D1/employee.rb
@@ -1,11 +1,17 @@
 class Employee
-  attr_reader :name, :title, :salary, :boss
+  attr_reader :name, :title, :salary
+  attr_accessor :boss
 
-  def initialize(name, title, salary, boss)
+  def initialize(name, title, salary, boss = nil)
     @name = name
     @title = title
     @salary = salary
+    self.boss = boss
+  end
+
+  def boss=(boss)
     @boss = boss
+    boss.add_employee(self) unless boss.nil?
   end
 
   def bonus(multiplier)
@@ -16,21 +22,47 @@ end
 class Manager < Employee
   attr_reader :employees
 
-  def initialize(name, title, salary, boss, employees)
+  def initialize(name, title, salary, boss = nil)
     super(name, title, salary, boss)
-    @employees = employees
+    @employees = []
+  end
+
+  def add_employee(subordinate)
+    employees << subordinate
+
+    subordinate
   end
 
   def bonus(multiplier)
-    total_employee_salary = @employees.reduce(0) { |acc, employee| acc += employee.salary }
-    total_employee_salary * multiplier
+    self.total_subsalary * multiplier
+  end
+
+  protected
+  
+  def total_subsalary
+    total_subsalary = 0
+    self.employees.each do |employee|
+      if employee.is_a?(Manager)
+        total_subsalary += employee.salary + employee.total_subsalary
+      else
+        total_subsalary += employee.salary
+      end
+    end
+
+    total_subsalary
   end
 end
 
-shawna = Employee.new("Shawna", "TA", 12000, "Darren")
-david = Employee.new("David", "TA", 10000, "Darren")
-darren = Manager.new("Darren", "TA Manager", 78000, "Ned", [shawna, david])
-ned = Manager.new("Ned", "Founder", 1000000, nil, [darren, shawna, david])
+shawna = Employee.new("Shawna", "TA", 12000)
+david = Employee.new("David", "TA", 10000)
+
+darren = Manager.new("Darren", "TA Manager", 78000)
+shawna.boss = darren
+david.boss = darren
+
+ned = Manager.new("Ned", "Founder", 1000000)
+darren.boss = ned
+
 
 p ned.bonus(5)
 p darren.bonus(4)
