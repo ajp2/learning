38799bd4243b67510f0e7cd81a991bb4b7c64801
Arvind <arvind.puvi@gmail.com>
2018-11-24 00:35:00 +1100

    Finish class inheritance

---
+++ Ruby/W2D1/employee.rb
@@ -0,0 +1,37 @@
+class Employee
+  attr_reader :name, :title, :salary, :boss
+
+  def initialize(name, title, salary, boss)
+    @name = name
+    @title = title
+    @salary = salary
+    @boss = boss
+  end
+
+  def bonus(multiplier)
+    @salary * multiplier
+  end
+end
+
+class Manager < Employee
+  attr_reader :employees
+
+  def initialize(name, title, salary, boss, employees)
+    super(name, title, salary, boss)
+    @employees = employees
+  end
+
+  def bonus(multiplier)
+    total_employee_salary = @employees.reduce(0) { |acc, employee| acc += employee.salary }
+    total_employee_salary * multiplier
+  end
+end
+
+shawna = Employee.new("Shawna", "TA", 12000, "Darren")
+david = Employee.new("David", "TA", 10000, "Darren")
+darren = Manager.new("Darren", "TA Manager", 78000, "Ned", [shawna, david])
+ned = Manager.new("Ned", "Founder", 1000000, nil, [darren, shawna, david])
+
+p ned.bonus(5)
+p darren.bonus(4)
+p david.bonus(3)
