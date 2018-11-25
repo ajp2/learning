98b62a967a44ac3e48f9948add1090543820c103
Arvind <arvind.puvi@gmail.com>
2018-11-26 00:41:57 +1100

    Add slideable and stepable modules with subclasses

--- Ruby/W2D1/chess/Board.rb
+++ Ruby/W2D1/chess/Board.rb
@@ -9,23 +9,21 @@ class Board
     place_pieces
   end
 
-  # call by using self[x, y] ??
-  # def [](pos)
-  #   puts "zxgaxfgzfgdflgjk"
-  #   x, y = pos
-  #   @board[x][y]
-  # end
+  def [](pos)
+    x, y = pos
+    @board[x][y]
+  end
 
-  # def []=(pos, value)
-  #   x, y = pos
-  #   @board[x][y] = value
-  # end
+  def []=(pos, value)
+    x, y = pos
+    @board[x][y] = value
+  end
 
   def place_pieces
     @board.each_with_index do |row, x|
       row.each_with_index do |tile, y|
         if x.between?(0, 1) || x.between?(6, 7)
-          @board[x][y] = Piece.new
+          self[[x, y]] = Piece.new
         end
       end
     end
@@ -34,10 +32,10 @@ class Board
   def move_piece(start_pos, end_pos)
     x1, y1 = start_pos
     x2, y2 = end_pos
-    raise ArgumentError.new("No piece at start_pos") if @board[x1][y1].nil?
+    raise ArgumentError.new("No piece at start_pos") if self[start_pos].nil?
     raise ArgumentError.new("end_pos is invalid") unless valid_pos?(end_pos)
     
-    @board[x2][y2], @board[x1][y1] = @board[x1][y1], nil
+    self[end_pos], self[start_pos] = self[start_pos], nil
   end
 
   def valid_pos?(pos)
--- Ruby/W2D1/chess/Piece.rb
+++ Ruby/W2D1/chess/Piece.rb
@@ -1,3 +1,192 @@
 class Piece
+  attr_reader :color, :board, :pos
+
+  def initialize(color, board, pos)
+    @color = color
+    @board = board
+    @pos = pos
+  end
+end
+
+
+class Rook < Piece
+  include SlidingPiece
+
+  def symbol
+    :rook
+  end
+
+  def move_dirs
+    horizontal_dirs
+  end
+end
+
+# rook = Rook.new(:white, "board", [0,0])
+# rook.moves
+#   calls rook.move_dirs
+
+
+class Bishop < Piece
+  include SlidingPiece
+
+  def symbol
+    :bishop
+  end
+
+  def move_dirs
+    diagonal_dirs
+  end
+end
+
+
+class Queen < Piece
+  include SlidingPiece
+
+  def symbol
+    :queen
+  end
+
+  def move_dirs
+    horizontal_dirs + diagonal_dirs
+  end
+end
+
+
+class Knight < Piece
+  include SteppingPiece
+
+  def symbol
+    :knight
+  end
+
+  def move_diffs
+    [
+      [-2, -1],
+      [-2, 1],
+      [-1, -2],
+      [1, -2],
+      [2, -1],
+      [2, 1],
+      [-1, 2],
+      [1, 2]
+    ]
+  end
+end
+
+class King < Piece
+  include SteppingPiece
+
+  def symbol
+    :king
+  end
+
+  def move_diffs
+    [
+      [-1, 0],
+      [1, 0],
+      [0, -1],
+      [0, 1]
+    ]
+  end
+end
+
+class Pawn < Piece
+
+end
+
+class NullPiece < Piece
+  include Singleton
+
+  def initialize
+
+  end
+end
+
+
+
+
+
+
+
+
+module SlidingPiece
+  HORIZONTAL_DIRS = [
+    [-1, 0],
+    [1, 0],
+    [0, -1],
+    [0, 1]
+  ]
+  DIAGONAL_DIRS = [
+    [-1, -1],
+    [-1. 1],
+    [1, -1],
+    [1, 1]
+  ]
+
+  def horizontal_dirs
+    HORIZONTAL_DIRS
+  end
+
+  def diagonal_dirs
+    DIAGONAL_DIRS
+  end
+
+  def moves
+    dirs = self.move_dirs
+    possible_moves = []
+    dirs.each do |dir|
+      possible_moves << move_in_dir(dir)
+    end
+
+    possible_moves
+  end
+
+  private
+  # if hit own piece, stop one position before
+  # if hit oppo piece, take the piece and stop there
+  # otherwise stop when the end of board is reached
+  def move_in_dir(dir)
+    hit_piece? = false
+    next_pos = self.pos
+
+    until hit_piece?
+      previous_pos = next_pos.dup
+      next_pos = grow_unblocked_moves_in_dir(next_pos, dir)
+
+      # Not a valid pos, move back. Can't go futher so break
+      if !(self.board.valid_pos?(next_pos))
+        next_pos = previous_pos
+        break
+      end
+
+      # Reached a piece
+      if self.board[next_pos] # change this after nil positions become objects
+        # Same color, move back
+        next_pos = previous_pos if self.color == self.board[next_pos].color
+        hit_piece? = true
+      end
+    end
+
+    next_pos
+  end
   
+  def grow_unblocked_moves_in_dir(x, y, dx, dy)
+    [x + dx, y + dy]
+  end
+end
+
+module SteppingPiece
+  def moves
+    possible_moves = []
+
+    self.move_diffs.each do |move|
+      new_pos = [self.pos[0] + move[0], self.pos[1] + move[1]]
+
+      if self.board.valid_pos?(new_pos) && self.color != self.board[new_pos].color
+        possible_moves << new_pos
+      end
+    end
+
+    possible_moves
+  end
 end
