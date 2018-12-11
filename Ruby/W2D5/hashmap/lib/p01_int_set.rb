785e926b880f2331af2069185aea5518f2185543
Arvind <arvind.puvi@gmail.com>
2018-12-11 14:38:28 +1100

    Complete phase 1

--- Ruby/W2D5/hashmap/lib/p01_int_set.rb
+++ Ruby/W2D5/hashmap/lib/p01_int_set.rb
@@ -1,22 +1,31 @@
 class MaxIntSet
   def initialize(max)
+    @store = Array.new(max + 1, false)
   end
 
   def insert(num)
+    validate!(num)
+    @store[num] = true
   end
 
   def remove(num)
+    validate!(num)
+    @store[num] = false
   end
 
   def include?(num)
+    validate!(num)
+    @store[num]
   end
 
   private
 
   def is_valid?(num)
+    num >= 0 && num < @store.length
   end
 
   def validate!(num)
+    raise "Out of bounds" unless is_valid?(num)
   end
 end
 
@@ -27,18 +36,22 @@ class IntSet
   end
 
   def insert(num)
+    self[num] << num unless include?(num)
   end
 
   def remove(num)
+    self[num].delete(num)
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -55,18 +68,29 @@ class ResizingIntSet
   end
 
   def insert(num)
+    if !include?(num)
+      resize! if count == num_buckets
+      self[num] << num
+      @count += 1
+    end
   end
 
   def remove(num)
+    if include?(num)
+      self[num].delete(num)
+      @count -= 1
+    end
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -74,5 +98,13 @@ class ResizingIntSet
   end
 
   def resize!
+    new_store = Array.new(num_buckets * 2) { Array.new }
+    @store.each do |bucket|
+      bucket.each do |el|
+        new_store[el % new_store.length] << el
+      end
+    end
+
+    @store = new_store
   end
 end
