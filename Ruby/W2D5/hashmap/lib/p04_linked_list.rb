587a74a88a2194fefd17421aa5b56ecb339dca50
Arvind <arvind.puvi@gmail.com>
2018-12-11 23:54:49 +1100

    Complete phase 4

--- Ruby/W2D5/hashmap/lib/p04_linked_list.rb
+++ Ruby/W2D5/hashmap/lib/p04_linked_list.rb
@@ -16,11 +16,21 @@ class Node
   def remove
     # optional but useful, connects previous link to next link
     # and removes self from list.
+    @prev.next = @next
+    @next.prev = @prev
+    @next, @prev = nil
   end
 end
 
 class LinkedList
+  include Enumerable
+
   def initialize
+    @head = Node.new
+    @tail = Node.new
+
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
@@ -29,34 +39,73 @@ class LinkedList
   end
 
   def first
+    @head.next
   end
 
   def last
+    @tail.prev
   end
 
   def empty?
+    @head.next == @tail && @tail.prev == @head
   end
 
   def get(key)
+    self.each do |current_node|
+      return current_node.val if current_node.key == key
+    end
+
+    nil
   end
 
   def include?(key)
+    self.each do |current_node|
+      return true if current_node.key == key
+    end
+
+    false
   end
 
   def append(key, val)
+    new_node = Node.new(key, val)
+
+    new_node.next = @tail
+    new_node.prev = last
+    last.next = @tail.prev = new_node
   end
 
   def update(key, val)
+    self.each do |current_node|
+      if current_node.key == key
+        current_node.val = val
+        return true
+      end
+    end
+
+    false
   end
 
   def remove(key)
+    self.each do |current_node|
+      if current_node.key == key
+        current_node.remove
+        return true
+      end
+    end
+
+    false
   end
 
-  def each
+  def each(&prc)
+    current_node = first
+    until current_node.next.nil?
+      prc.call(current_node)
+      current_node = current_node.next
+    end
   end
 
   # uncomment when you have `each` working and `Enumerable` included
-  # def to_s
-  #   inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
-  # end
+  def to_s
+    inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
+  end
 end
