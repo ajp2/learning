b822177a48004f679d2d6c82e32094e497ba0e3d
Arvind <arvind.puvi@gmail.com>
2018-12-12 00:10:30 +1100

    Complete phase 5

--- Ruby/W2D5/hashmap/lib/p05_hash_map.rb
+++ Ruby/W2D5/hashmap/lib/p05_hash_map.rb
@@ -1,6 +1,8 @@
 require_relative 'p04_linked_list'
 
 class HashMap
+  include Enumerable
+
   attr_accessor :count
 
   def initialize(num_buckets = 8)
@@ -9,27 +11,45 @@ class HashMap
   end
 
   def include?(key)
+    @store[bucket(key)].include?(key)
   end
 
   def set(key, val)
+    if include?(key)
+      @store[bucket(key)].update(key, val)
+    else
+      resize! if @count >= num_buckets
+      @store[bucket(key)].append(key, val)
+      @count += 1
+    end
   end
 
   def get(key)
+    @store[bucket(key)].get(key)
   end
 
   def delete(key)
+    if include?(key)
+      @store[bucket(key)].remove(key)
+      @count -= 1
+    end
   end
 
-  def each
+  def each(&prc)
+    @store.each do |linked_list|
+      linked_list.each do |node|
+        prc.call(node.key, node.val)
+      end
+    end
   end
 
   # uncomment when you have Enumerable included
-  # def to_s
-  #   pairs = inject([]) do |strs, (k, v)|
-  #     strs << "#{k.to_s} => #{v.to_s}"
-  #   end
-  #   "{\n" + pairs.join(",\n") + "\n}"
-  # end
+  def to_s
+    pairs = inject([]) do |strs, (k, v)|
+      strs << "#{k.to_s} => #{v.to_s}"
+    end
+    "{\n" + pairs.join(",\n") + "\n}"
+  end
 
   alias_method :[], :get
   alias_method :[]=, :set
@@ -41,9 +61,18 @@ class HashMap
   end
 
   def resize!
+    new_store = Array.new(num_buckets * 2) { LinkedList.new }
+    @store.each do |linked_list|
+      linked_list.each do |node|
+        new_store[node.key.hash % new_store.length].append(node.key, node.val)
+      end
+    end
+
+    @store = new_store
   end
 
   def bucket(key)
     # optional but useful; return the bucket corresponding to `key`
+    key.hash % num_buckets
   end
 end
