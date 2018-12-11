db242aa0962ea41bf1400c4af745629c7c519780
Arvind <arvind.puvi@gmail.com>
2018-12-11 18:55:39 +1100

    Finish phase 2

--- Ruby/W2D5/hashmap/lib/p02_hashing.rb
+++ Ruby/W2D5/hashmap/lib/p02_hashing.rb
@@ -4,11 +4,17 @@ end
 
 class Array
   def hash
+    acc = self[0] || 0
+    (1...self.length).each do |idx|
+      acc = acc ^ (self[idx] + idx)
+    end
+    acc.hash
   end
 end
 
 class String
   def hash
+    self.chars.map { |char| char.ord }.hash
   end
 end
 
@@ -16,6 +22,13 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    hash_arr = self.entries.flatten.map do |val|
+      if val.is_a?(Integer)
+        val
+      else
+        val.to_s.ord
+      end
+    end
+    hash_arr.sort.hash
   end
 end
