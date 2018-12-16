6e692d1d4b82853e685e9a5757f003e4f2288507
Arvind <arvind.puvi@gmail.com>
2018-12-16 18:44:49 +1100

    Finish easy queries in AA questions project

--- SQL/W3D2/AA_questions/users.rb
+++ SQL/W3D2/AA_questions/users.rb
@@ -1,15 +1,6 @@
 require 'sqlite3'
 require 'singleton'
-
-class QuestionsDatabase < SQLite3::Database
-  include Singleton
-
-  def initialize
-    super('questions.db')
-    self.type_translation = true
-    self.results_as_hash = true
-  end
-end
+require_relative 'base_model'
 
 class User
   attr_accessor :fname, :lname
@@ -47,6 +38,14 @@ class User
     @fname = options['fname']
     @lname = options['lname']
   end
+
+  def authored_questions
+    Question.find_by_author_id(@id)
+  end
+
+  def authored_replies
+    Reply.find_by_user_id(@id)
+  end
 end
 
 class Question
@@ -66,11 +65,43 @@ class Question
     Question.new(data.first)
   end
 
+  def self.find_by_author_id(author_id)
+    data = QuestionsDatabase.instance.execute(<<-SQL, author_id)
+      SELECT
+        *
+      FROM
+        questions
+      WHERE
+        author_id = ?
+    SQL
+
+    return nil if data.length == 0
+    data.map { |datum| Question.new(datum) }
+  end
+
   def initialize(options)
     @id = options['id']
     @title = options['title']
     @body = options['body']
     @author_id = options['author_id']
+  end
+
+  def author
+    data = QuestionsDatabase.instance.execute(<<-SQL, @author_id)
+      SELECT
+        *
+      FROM
+        users
+      WHERE
+        id = ?
+    SQL
+
+    return nil if data.length == 0
+    User.new(data.first)
+  end
+
+  def replies
+    Reply.find_by_question_id(@id)
   end
 end
 
@@ -115,12 +146,98 @@ class Reply
     Reply.new(data.first)
   end
 
+  def self.find_by_user_id(user_id)
+    data = QuestionsDatabase.instance.execute(<<-SQL, user_id)
+      SELECT
+        *
+      FROM
+        replies
+      WHERE
+        user_id = ?
+    SQL
+
+    return nil if data.length == 0
+    data.map { |datum| Reply.new(datum) }
+  end
+
+  def self.find_by_question_id(question_id)
+    data = QuestionsDatabase.instance.execute(<<-SQL, question_id)
+      SELECT
+        *
+      FROM
+        replies
+      WHERE
+        question_id = ?
+    SQL
+
+    return nil if data.length == 0
+    data.map { |datum| Reply.new(datum) }
+  end
+
   def initialize(options)
     @id = options['id']
     @question_id = options['question_id']
     @reply_id = options['reply_id']
     @user_id = options['user_id']
     @body = options['body']
+  end
+
+  def author
+    data = QuestionsDatabase.instance.execute(<<-SQL, self.user_id)
+      SELECT
+        *
+      FROM
+        users
+      WHERE
+        id = ?
+    SQL
+
+    return nil if data.length == 0
+    User.new(data.first)
+  end
+
+  def question
+    data = QuestionsDatabase.instance.execute(<<-SQL, self.question_id)
+      SELECT
+        *
+      FROM
+        questions
+      WHERE
+        id = ?
+    SQL
+
+    return nil if data.length == 0
+    Question.new(data.first)
+  end
+
+  def parent_reply
+    return nil if self.reply_id.nil?
+
+    data = QuestionsDatabase.instance.execute(<<-SQL, self.reply_id)
+      SELECT
+        *
+      FROM
+        replies
+      WHERE
+        id = ?
+    SQL
+
+    return nil if data.length == 0
+    Reply.new(data.first)
+  end
+
+  def child_replies
+    data = QuestionsDatabase.instance.execute(<<-SQL, @id)
+      SELECT
+        *
+      FROM
+        replies
+      WHERE
+        reply_id = ?
+    SQL
+
+    return nil if data.length == 0
+    data.map { |datum| Reply.new(datum) }
   end
 end
 
