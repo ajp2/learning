86624efeb47618f75820e0ca8701e6c6b8fbe3bd
Arvind <arvind.puvi@gmail.com>
2018-12-31 18:53:49 +1100

    Complete phase 1e to 1i and phase 2

--- SQL/W3D5/ActiveRecordLite/lib/01_sql_object.rb
+++ SQL/W3D5/ActiveRecordLite/lib/01_sql_object.rb
@@ -41,20 +41,41 @@ class SQLObject
 
   def self.all
     # ...
+    all_records = DBConnection.execute(<<-SQL)
+      SELECT
+        #{self.table_name}.*
+      FROM
+        #{self.table_name}
+    SQL
+
+    self.parse_all(all_records)
   end
 
   def self.parse_all(results)
     # ...
+    results.map do |result|
+      self.new(result)
+    end
   end
 
   def self.find(id)
     # ...
+    record_with_id = DBConnection.execute(<<-SQL, id)
+      SELECT
+        #{self.table_name}.*
+      FROM
+        #{self.table_name}
+      WHERE
+        id = ?
+    SQL
+
+    self.parse_all(record_with_id).first
   end
 
   def initialize(params = {})
     # ...
     params.each do |key, val|
-      raise "unknown attribute '#{key}'" unless self.class.columns.include?(key)
+      raise "unknown attribute '#{key}'" unless self.class.columns.include?(key.to_sym)
       self.send(key.to_s + "=", val)
     end
   end
@@ -66,21 +87,40 @@ class SQLObject
 
   def attribute_values
     # ...
+    self.class.columns.map { |col| self.send(col) }
   end
 
   def insert
     # ...
+    col_names = self.class.columns.join(",")
+    question_marks = (["?"] * self.class.columns.length).join(",")
+
+    DBConnection.execute(<<-SQL, attribute_values)
+      INSERT INTO
+        #{self.class.table_name} (#{col_names})
+      VALUES
+        (#{question_marks})
+    SQL
+
+    self.id = DBConnection.last_insert_row_id
   end
 
   def update
     # ...
+    set_line = self.class.columns.map { |col| "#{col} = ?"}.join(",")
+
+    DBConnection.execute(<<-SQL, attribute_values, self.id)
+      UPDATE
+        #{self.class.table_name}
+      SET
+        #{set_line}
+      WHERE
+        id = ?
+    SQL
   end
 
   def save
     # ...
+    self.id.nil? ? self.insert : self.update
   end
-end
-
-class Cat < SQLObject
-
 end
--- SQL/W3D5/ActiveRecordLite/lib/02_searchable.rb
+++ SQL/W3D5/ActiveRecordLite/lib/02_searchable.rb
@@ -4,9 +4,22 @@ require_relative '01_sql_object'
 module Searchable
   def where(params)
     # ...
+    where_line = params.keys.map { |k| "#{k} = ?" }.join(" AND ")
+
+    records = DBConnection.execute(<<-SQL, params.values)
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+      WHERE
+        #{where_line}
+    SQL
+
+    self.parse_all(records)
   end
 end
 
 class SQLObject
   # Mixin Searchable here...
+  extend Searchable
 end
