124dc60c5dcbb734766fcfeab57fb5700d8962f6
Arvind <arvind.puvi@gmail.com>
2019-01-10 17:33:40 +1100

    Add authentication for reddit clone project

---
+++ Rails/W5D2/reddit_clone/app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+    if @user
+      login_user!(@user)
+      redirect_to user_url(@user)
+    else
+      flash[:errors] = "Invalid username or password"
+      redirect_to new_session_url
+    end
+  end
+
+  def new
+    render 'new'
+  end
+
+  def destroy
+    current_user.reset_session_token!
+    session[:session_token] = nil
+    redirect_to new_session_url
+  end
+end
---
+++ Rails/W5D2/reddit_clone/app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      login_user!(@user)
+      redirect_to user_url(@user)
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render 'new'
+    end
+  end
+
+  def new
+    render 'new'
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render 'show'
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+end
