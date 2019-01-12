d9ba1cc91716f1c5b565dd7f332edbc92d036fa5
Arvind <arvind.puvi@gmail.com>
2019-01-13 10:43:24 +1100

    Complete phase 2 and 3

--- Rails/W5D3/rails_lite/lib/controller_base.rb
+++ Rails/W5D3/rails_lite/lib/controller_base.rb
@@ -8,25 +8,50 @@ class ControllerBase
 
   # Setup the controller
   def initialize(req, res)
+    @req = req
+    @res = res
+    @already_built_response = false
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response
   end
 
   # Set the response status code and header
   def redirect_to(url)
+    if already_built_response?
+      raise "Double render"
+    end
+
+    res.status = 302
+    res['Location'] = url
+    res.finish
+    @already_built_response = true
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    if already_built_response?
+      raise "Double render"
+    end
+
+    res['Content-Type'] = content_type
+    res.write(content)
+    res.finish
+    @already_built_response = true
   end
 
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
+    main_folder = File.dirname(File.dirname(__FILE__))
+    file_path = File.join(main_folder, "views", self.class.name.underscore, template_name.to_s + ".html.erb")
+    file_info = File.read(file_path)
+    content = ERB.new(file_info).result(binding)
+    render_content(content, 'text/html')
   end
 
   # method exposing a `Session` object
@@ -37,4 +62,3 @@ class ControllerBase
   def invoke_action(name)
   end
 end
-
