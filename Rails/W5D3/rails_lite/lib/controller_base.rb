67f68316be7de9991fe092923a5191572197b186
Arvind <arvind.puvi@gmail.com>
2019-01-14 00:03:21 +1100

    Complete phase 5 and 6

--- Rails/W5D3/rails_lite/lib/controller_base.rb
+++ Rails/W5D3/rails_lite/lib/controller_base.rb
@@ -7,9 +7,10 @@ class ControllerBase
   attr_reader :req, :res, :params
 
   # Setup the controller
-  def initialize(req, res)
+  def initialize(req, res, route_params)
     @req = req
     @res = res
+    @params = req.params.merge(route_params)
     @already_built_response = false
   end
 
@@ -63,5 +64,7 @@ class ControllerBase
 
   # use this with the router to call action_name (:index, :show, :create...)
   def invoke_action(name)
+    self.send(name)
+    render(name) unless already_built_response?
   end
 end
