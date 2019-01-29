871d895655b6bce298184761d4ecc9e5ecbbbf61
Arvind <arvind.puvi@gmail.com>
2019-01-29 16:57:51 +1100

    Add store and initial reducers

--- React/W7D1/todos/todo_redux.jsx
+++ React/W7D1/todos/todo_redux.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import configureStore from "./frontend/store/store";
 
 document.addEventListener("DOMContentLoaded", () => {
+  const store = configureStore();
+  window.store = store;
+  
   const root = document.getElementById("root");
   ReactDOM.render(<h1>Todos App</h1>, root)
 });
