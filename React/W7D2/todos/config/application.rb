16182aff509225cb77a4df417a760ef3d64cd777
Arvind <arvind.puvi@gmail.com>
2019-01-30 23:30:35 +1100

    Add todos controller and routes

--- React/W7D2/todos/config/application.rb
+++ React/W7D2/todos/config/application.rb
@@ -15,5 +15,8 @@ module Todos
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    initializer(:remove_activestorage_routes, after: :add_routing_paths) {|app|
+      app.routes_reloader.paths.delete_if {|path| path =~ /activestorage/}}
   end
 end
