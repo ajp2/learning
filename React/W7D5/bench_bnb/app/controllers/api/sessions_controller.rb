b14dfa5f820264044f72a8fb64f354eda93beab1
Arvind <arvind.puvi@gmail.com>
2019-02-06 19:05:12 +1100

    Add ability to login and logout

--- React/W7D5/bench_bnb/app/controllers/api/sessions_controller.rb
+++ React/W7D5/bench_bnb/app/controllers/api/sessions_controller.rb
@@ -1,9 +1,23 @@
 class Api::SessionsController < ApplicationController
   def create
-
+    @user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+    if @user
+      login_user!(@user)
+      render "api/users/show"
+    else
+      render json: { error: "Invalid credentials" }, status: 422
+    end
   end
 
   def destroy
-    
+    if current_user
+      current_user.reset_session_token!
+      session[:session_token] = nil;
+      render json: {}
+    else
+      render json: { error: "Not logged in" }, status: 404
   end
 end
--- React/W7D5/bench_bnb/app/models/user.rb
+++ React/W7D5/bench_bnb/app/models/user.rb
@@ -4,6 +4,26 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
   validates :password_digest, presence: { message: "Password can't be blank" }
   validates :password, length: { minimum: 6, allow_nil: true }
+  after_initialize :ensure_session_token
+
+  def self.find_by_credentials(username, password)
+    @user = User.find_by(username: username)
+    @user && @user.is_password?(password) ? @user : nil
+  end
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64(16)
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    self.save
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
 
   def password=(password)
     @password = password
