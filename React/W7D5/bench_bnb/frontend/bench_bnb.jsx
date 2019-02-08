4bb73b347e5d6983b8a3f55bd073402f23082714
Arvind <arvind.puvi@gmail.com>
2019-02-08 13:56:35 +1100

    Define bench actions and reducers

--- React/W7D5/bench_bnb/frontend/bench_bnb.jsx
+++ React/W7D5/bench_bnb/frontend/bench_bnb.jsx
@@ -4,6 +4,7 @@ import configureStore from "./store/store";
 import Root from "./components/root";
 
 import { signup } from "./util/session_api_util";
+import { fetchBenches } from "./actions/bench_actions";
 
 document.addEventListener("DOMContentLoaded", () => {
   let store;
